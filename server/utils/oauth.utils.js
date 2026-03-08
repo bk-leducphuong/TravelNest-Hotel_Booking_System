531a5009c33d7d868d21b199c997a24003504b2f
bkleducphuong <bkleducphuong@gmail.com>
2026-03-08 23:37:11 +0700

    Add bullmq worker health check

--- server/utils/oauth.utils.js
+++ server/utils/oauth.utils.js
@@ -1,10 +1,10 @@
 function getCallbackUrl(provider) {
-  const baseUrl = process.env.API_BASE_URL || '';
+  const baseUrl = process.env.EXTERNAL_BASE_URL || '';
   if (!baseUrl) {
     // Relative path for dev/proxy setups
     return `/api/v1/auth/${provider}/callback`;
   }
-  return `${baseUrl}/api/v1/auth/${provider}/callback`;
+  return `${baseUrl}/auth/${provider}/callback`;
 }
 
 module.exports = {
